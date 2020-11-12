efd399e6dd2bb96fd8c61413746e9fe1440af978
Omer Acar <omeracar0@gmail.com>
2020-11-12 18:27:49 -0500

    blog page created navbar header created

---
+++ components/Header.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <div class="navBar flex m-auto z-999 sticky h-auto my-12">
+        <h2>Hello world</h2>
+    <nav class = "sm:flex flex-col sm:flex-row sm:w-auto w-full order-last sm:order-none my-4 sm:my-0 hidden"> 
+        <ul class="flex justify-between"> 
+            <li v-for="item in mainNavItems" :key="item.id">
+            <NuxtLink :to="item.slug" class="relative text-indigo-900 border-b border-transparent hover:text-indigo-300 ml-0 sm:ml-8 mt-3 sm:mt-0">
+
+        {{item.title}}
+        </NuxtLink>
+            </li> 
+    </ul>
+
+    </nav>
+  </div>
+</template>
+
+<script>
+const mainNavItems = [
+  {
+    title: 'About Me',
+    slug: '/'
+  },
+  {
+    title: 'Resume',
+    slug: '/resume/'
+  },
+  {
+    title: 'Blog',
+    slug: '/blog/'
+  },
+  {
+    title: 'Contact Me',
+    slug: '/contact/'
+  }
+];
+export default {
+    data: function () {
+        return {
+        mainNavItems: mainNavItems,
+        show: true
+        }
+    }   ,
+    methods: {
+        toggleNavbar(){
+            this.show = !this.show
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,70 @@
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      // fetch our article here
+        const article = await $content('articles', params.slug).fetch()
+        const [prev, next] = await $content('articles')
+            .only(['title','slug'])
+            .sortBy('createdAt','asc')
+            .surround(params.slug)
+            .fetch()
+      return { 
+          article ,
+          prev,
+          next}
+    },
+    methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    }
+ }
+  }
+</script>
+<style>
+  .nuxt-content h1 {
+    font-weight: bold;
+    font-size: 22px;
+    /* background-color: black; */
+  }
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 20px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+  .icon.icon-link {
+      background-image: url('~assets/svg/icon-hashtag.svg');
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-size: 20px 20px;
+  }
+</style>
+
+<template>
+  <article>
+    <h1 class="bg-blue-500">{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <nav>
+      <ol class="list-decimal">
+        <li v-for="link of article.toc" :key="link.id" :class="{ 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 }"
+>
+         <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+        </li>
+    </ol>
+    </nav>
+
+    <img :src="article.img" :alt="article.alt" />
+
+    <nuxt-content :document="article" />
+
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
+    <author :author="article.author"/>
+
+    <prev-next :prev="prev" :next="next" class="flex justify-between my-2"/>
+    <!-- <pre> {{ article }} </pre> -->
+  </article>
+</template>
